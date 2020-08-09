a9dea036f42e51b992f5413d0b40f6f0234f2f3e
Marta Kruszewska <martakruszewska007@gmail.com>
2020-08-09 08:12:18 +0200

    add Pickle

--- Currency_calculator.py
+++ Currency_calculator.py
@@ -4,13 +4,25 @@ from flask import request, redirect, url_for
 import requests
 import csv
 import pickle
+import time
 
 
 app = Flask(__name__)
 
 def get_data():
-    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-    return response.json()[0]['rates']
+    try:
+        with open('datadump.pickle', 'rb') as cache:
+            timestamp, data = pickle.load(cache)
+            if timestamp < time.time() - 5:
+                raise ValueError("Stale cache")
+            return data
+    except (FileNotFoundError, ValueError) as error:
+        print(error)
+        response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+        data = response.json()[0]['rates']
+        with open('datadump.pickle', 'wb') as cache:
+            pickle.dump((time.time(), data), cache)
+        return data
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -23,6 +35,7 @@ def currency_calculator():
         csv_writer.writeheader()
         for item in data:
             csv_writer.writerow(item)
+
     
     codes = []
     for item in data:
